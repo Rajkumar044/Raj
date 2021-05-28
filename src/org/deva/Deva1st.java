ffb8678cf15e84e7cf7b546b2cfdcaa38a3a1562
Rajkumar044 <rajdeva990@gmail.com>
2021-05-28 19:13:34 +0530

    1st push

---
+++ src/org/deva/Deva1st.java
@@ -0,0 +1,12 @@
+package org.deva;
+
+public class Deva1st {
+	private void age() {
+		System.out.println("21");
+	}
+	public static void main(String[] args) {
+		Deva1st a=new Deva1st();
+		a.age();
+	}
+
+}
